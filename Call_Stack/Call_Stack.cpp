a20d54471b222fea8cea3c8d227ca017d368d766
Beyah96 <bahsaid96@gmail.com>
2024-09-29 23:51:45 +0200

    Call stack lesson

---
+++ Call_Stack/Call_Stack.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std; 
+
+void Function4() {
+    cout << "Hi I'm function4 " << endl; 
+} 
+void Function3() {
+    Function4(); 
+} 
+void Function2_version_2() {
+    Function3();
+}
+void Function2() {
+    Function3(); 
+} 
+void Function1() {
+    Function2(); 
+} 
+int main() {     
+Function1(); 
+return 0;
+}
